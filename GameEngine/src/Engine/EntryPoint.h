c01f9b4ac99e5bb743897bf2cd61ef073b8cde55
unknown <mister.94@tlen.pl>
2019-07-05 19:50:37 +0200

    Premake and Log

--- GameEngine/src/Engine/EntryPoint.h
+++ GameEngine/src/Engine/EntryPoint.h
@@ -6,6 +6,12 @@ extern GameEngine::Application* GameEngine::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	GameEngine::Log::Init();
+	GE_CORE_WARN("Warn log");
+	int a = 5;
+	GE_INFO("Info log Var={0}", a);
+
+
 	auto app = GameEngine::CreateApplication();
 	app->Run();
 	delete app;
